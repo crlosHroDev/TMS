02266acec3d1d96f1280f50e59ddaf9402ec19c1
crlosHroDev <chernandezDev@gmail.com>
2018-12-15 13:09:43 -0500

    navBar finalizado

--- src/navBar/components/NavBar.js
+++ src/navBar/components/NavBar.js
@@ -1,23 +1,25 @@
 import React from 'react';
 import './NavBar.css';
+import image from '../../images/logo.jpg'
 
 function NavBar(){
     return(
         <nav>
             <ul>
-            <li>
-                <a href='#'>¿Quienes somos?</a>
-                <ul className='subNav'>
-                    <li><a href='#'>Misión</a></li>
-                    <li><a href='#'>Visión</a></li>
-                    <li><a href='#'>Valores Corporativos</a></li>
-                </ul>
-            </li>
-            <li><a href='#'>Servicios</a></li>
-            <div className='login'>
-                <img src='https://img.icons8.com/bubbles/50/000000/gender-neutral-user.png'></img>
-                <li><a href='#'>Registrarme/Iniciar Sesión</a></li>
-            </div>
+                <li><img src={Image}></img></li>
+                <li className='history'>
+                    <a href='#'>¿Quienes somos?</a>
+                    <ul className='subNav'>
+                        <li><a href='#'>Misión</a></li>
+                        <li><a href='#'>Visión</a></li>
+                        <li><a href='#'>Valores Corporativos</a></li>
+                    </ul>
+                </li>
+                <li><a href='#'>Servicios</a></li>
+                <div className='login'>
+                    <img src='https://img.icons8.com/bubbles/50/000000/gender-neutral-user.png'></img>
+                    <li><a href='#'>Registrarme/Iniciar Sesión</a></li>
+                </div>
             </ul>
         </nav>
     )
